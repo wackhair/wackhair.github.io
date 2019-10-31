dced23fbf555bca6165ef71ad0095953e1d1671a
Emilowiz <Emil-josefsen023@hotmail.com>
2019-10-31 15:02:14 +0100

    aw

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - [Tobis homepage and tat](https://tjobthetub.github.io)
 - [Ma own homepage and tat](website/index.html)
 - [My stickmaaaan lookin thickboi](p5.js/dab)
+- [My Advanceed stickmans](p5.jsss/dab2)
---
+++ p5.jsss/dab2/sketch.js
@@ -0,0 +1,40 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+
+
+  let x = mouseX;
+  let y = mouseY;
+  let headSize = 0.5 * mouseX;
+  let hr = headSize / 2;
+  let manHeight = 150;
+
+  strokeWeight(0.05 * x)
+  fill(100,100,0)
+  circle(x, y, headSize)
+  strokeWeight(0.05 * x)
+  line(x, y + hr, x, y + headSize + manHeight);
+  strokeWeight(0.005 * x)
+
+  
+  if (eyeFlag) {
+    fill(255,0,0);
+  } else {
+    fill(0, 255, 0);
+  }
+
+  circle(x - hr / 2, y - hr / 2, 0.05 * x)
+  circle(x + hr / 2, y - hr / 2, 0.05 * x)
+  
+}
+
+let eyeFlag = false;
+
+
+function mousePressed() {
+  eyeFlag = !eyeFlag
+
+}
