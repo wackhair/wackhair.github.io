94570fe7844077fbe67c364d2f68d0e706c22b9f
Emilowiz <Emil-josefsen023@hotmail.com>
2019-10-31 11:23:15 +0100

    aåwd

---
+++ p5.js/dab/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+circle(200, 110, 75);
+line(197,200,150,150)
+line(203,200,250,150)
+line(200,150,200,300);
+line(200,300,250,350)
+line(200,300,150,350)
+circle(185,105,5)
+circle(200,105,5)
+line(195,125,205,125)
+  
+  strokeWeight(6)}
