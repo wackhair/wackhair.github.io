c952ece4456be15df612ba468a14430db4e09381
Emilowiz <Emil-josefsen023@hotmail.com>
2019-11-14 14:51:52 +0100

    asd

--- Spinning/spinn.js
+++ Spinning/spinn.js
@@ -1,11 +1,17 @@
 let img;
 let imgSize = 512
+let vinkel = -1
 function draw() {
     createCanvas(windowWidth, windowHeight);
-    image(img, windowWidth/2-imgSize/2, windowHeight/2-imgSize/2);
+    translate(windowWidth/2,windowHeight/2)
+    rotate(0.01 * vinkel);
+    imageMode(CENTER)
+    image(img, 0,0);
+    vinkel --
 }
 
 function preload() {
     img = loadImage('Spiral.png');
+
 
 }
