054b81ae82d3b5977c801e1e79151fa75a555053
Emilowiz <Emil-josefsen023@hotmail.com>
2019-11-14 15:13:29 +0100

    asd

--- Spinning/spinn.js
+++ Spinning/spinn.js
@@ -1,13 +1,15 @@
 let img;
 let imgSize = 512
-let vinkel = -1
+let vinkel = 1
 function draw() {
     createCanvas(windowWidth, windowHeight);
     translate(windowWidth/2,windowHeight/2)
     rotate(0.01 * vinkel);
     imageMode(CENTER)
     image(img, 0,0);
-    vinkel --
+    vinkel = mouseX * 2
+
+
 }
 
 function preload() {
