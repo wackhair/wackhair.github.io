c8bff871454b9687b2a51664c67f87cf38b6b069
Emilowiz <Emil-josefsen023@hotmail.com>
2019-11-14 13:58:56 +0100

    sdf

--- Spinning/spinn.js
+++ Spinning/spinn.js
@@ -1,9 +1,11 @@
 let img;
-function setup() {
-    createCanvas(400, 400);
-    image(img, 0, 0);
+let imgSize = 512
+function draw() {
+    createCanvas(windowWidth, windowHeight);
+    image(img, windowWidth/2-imgSize/2, windowHeight/2-imgSize/2);
 }
 
 function preload() {
     img = loadImage('Spiral.png');
+
 }
