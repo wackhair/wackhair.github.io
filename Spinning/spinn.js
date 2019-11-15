f9562ada200f9580338ff0c381008287ae409ba8
Emilowiz <Emil-josefsen023@hotmail.com>
2019-11-16 00:37:04 +0100

    asd

--- Spinning/spinn.js
+++ Spinning/spinn.js
@@ -1,10 +1,11 @@
 let img;
 let imgSize = 512
 let vinkel = 1
+let speed = 0.01
 function draw() {
-    createCanvas(windowWidth, windowHeight);
-    translate(windowWidth/2,windowHeight/2)
-    rotate(0.01 * vinkel);
+    createCanvas(windowWidth-25, windowHeight-25);
+    translate(mouseX, mouseY)
+    rotate(speed * vinkel);
     imageMode(CENTER)
     image(img, 0,0);
       if(keyIsDown(LEFT_ARROW)) {
@@ -14,7 +15,15 @@ function draw() {
         vinkel --;
       }
     } 
+    function keyPressed() {
 
+      if (keyCode === UP_ARROW){
+        speed = speed * 2;
+      }
+      if (keyCode === DOWN_ARROW){
+        speed = 0.01;
+      }
+    } 
 function preload() {
     img = loadImage('Spiral.png');
 
