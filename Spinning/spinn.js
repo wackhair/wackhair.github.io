dc95face2e2002402de5950f7e96e01f4f28a7eb
Emilowiz <Emil-josefsen023@hotmail.com>
2019-11-16 01:07:26 +0100

    asd

--- Spinning/spinn.js
+++ Spinning/spinn.js
@@ -4,33 +4,34 @@ let vinkel = 1
 let speed = 0.01
 function draw() {
   let sideArrows = "Use arrows to the side for Spin.."
-  let upAndDownArrows = "Use arrows to the up and the down for Speeeeed!"
-  createCanvas(windowWidth - 25, windowHeight - 25);
-  textSize(15);
-  text(sideArrows, 3, 3, 3000, 3000)
+  let  upAndDownArrows = "Use arrows to the up and the down for Speeeeed!"
+    createCanvas(windowWidth-25, windowHeight-25);
+    textSize(15);
+       text(sideArrows, 3, 3, 3000, 3000)
   text(upAndDownArrows, 3, 20, 3000, 3000)
-  translate(mouseX, mouseY)
-  rotate(speed * vinkel);
-  imageMode(CENTER)
-  image(img, 0, 0);
-  if (keyIsDown(LEFT_ARROW)) {
-    vinkel++;
-  }
-  if (keyIsDown(RIGHT_ARROW)) {
-    vinkel--;
-  }
+  //Try to make a *X counter for speed
+    translate(mouseX, mouseY)
+    rotate(speed * vinkel);
+    imageMode(CENTER)
+    image(img, 0,0);
+      if(keyIsDown(LEFT_ARROW)) {
+        vinkel ++;
+      }
+      if(keyIsDown(RIGHT_ARROW)) {
+        vinkel --;
+      }
 
-}
-function keyPressed() {
+    } 
+    function keyPressed() {
 
-  if (keyCode === UP_ARROW) {
-    speed = speed * 2;
-  }
-  if (keyCode === DOWN_ARROW) {
-    speed = speed / 2;
-  }
-}
+      if (keyCode === UP_ARROW){
+        speed = speed * 2;
+      }
+      if (keyCode === DOWN_ARROW){
+        speed = speed / 2;
+      }
+    } 
 function preload() {
-  img = loadImage('Spiral.png');
+    img = loadImage('Spiral.png');
 
 }
